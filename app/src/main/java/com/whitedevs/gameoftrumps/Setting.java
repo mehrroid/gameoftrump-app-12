c99104ef07bcc50a063beb7986334f318212bfcc
n4554 <assemuni@gmail.com>
2017-05-29 22:54:35 +0430

    Merge branch 'master' of C:\Users\nixan\Documents\GitHub\gameoftrump-app-12 with conflicts.

--- app/src/main/java/com/whitedevs/gameoftrumps/Setting.java
+++ app/src/main/java/com/whitedevs/gameoftrumps/Setting.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -17,30 +19,97 @@ public class Setting extends AppCompatActivity {
         TextView txtCirclef;
         TextView txtCirclef2;
         TextView txtCirclef3;
-        TextView txtCircle1;
-        TextView txtCircle2;
-        TextView txtCircle3;
+        TextView txtviewblue;
+        TextView txtviewpink;
+        TextView txtviewblack;
         Typeface font;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
         font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
 
-        txtVolume= (TextView) findViewById(R.id.txtVolume);
+        txtVolume= (TextView) findViewById(R.id.txtVolumeIcon);
         txtVolume.setTypeface(font);
-        txtVibration= (TextView) findViewById(R.id.txtVibration);
+        txtVibration= (TextView) findViewById(R.id.txtVibrationIcon);
         txtVibration.setTypeface(font);
-        txtCirclef= (TextView) findViewById(R.id.circlef);
-        txtCirclef.setTypeface(font);
-        txtCirclef2= (TextView) findViewById(R.id.circlef2);
-        txtCirclef2.setTypeface(font);
-        txtCirclef3= (TextView) findViewById(R.id.circlef3);
-        txtCirclef3.setTypeface(font);
+        txtviewblue= (TextView) findViewById(R.id.blueapp);
+        txtviewblue.setTypeface(font);
+        txtviewpink= (TextView) findViewById(R.id.pinkapp);
+        txtviewpink.setTypeface(font);
+        txtviewblack= (TextView) findViewById(R.id.blackapp);
+        txtviewblack.setTypeface(font);
 //        txtCircle1= (TextView) findViewById(R.id.circle1);
 //        txtCircle1.setTypeface(font);
 //        txtCircle2= (TextView) findViewById(R.id.circle2);
 //        txtCircle2.setTypeface(font);
 //        txtCircle3= (TextView) findViewById(R.id.circle3);
 //        txtCircle3.setTypeface(font);
+
+
+        txtviewblue.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+
+        txtviewpink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                LinearLayout bgSetting = (LinearLayout) findViewById(R.id.bgSetting);
+                bgSetting.setBackgroundResource(R.color.setPinkkBg);
+                TextView tv = (TextView) findViewById(R.id.txtVolumeIcon);
+                tv.setTextColor(getResources().getColor(R.color.setPinkfont));
+                tv = (TextView) findViewById(R.id.txtVolumeText);
+                tv.setTextColor(getResources().getColor(R.color.setPinkfont));
+                tv = (TextView) findViewById(R.id.txtVibrationIcon);
+                tv.setTextColor(getResources().getColor(R.color.setPinkfont));
+                tv = (TextView) findViewById(R.id.txtVibrationText);
+                tv.setTextColor(getResources().getColor(R.color.setPinkfont));
+                tv = (TextView) findViewById(R.id.tvSky);
+                tv.setTextColor(getResources().getColor(R.color.setPinkfont));
+                tv = (TextView) findViewById(R.id.tvStrawberry);
+                tv.setTextColor(getResources().getColor(R.color.setPinkfont));
+                tv = (TextView) findViewById(R.id.tvNight);
+                tv.setTextColor(getResources().getColor(R.color.setPinkfont));
+
+            }
+
+        });
+
+        txtviewblack.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                LinearLayout bgSetting = (LinearLayout) findViewById(R.id.bgSetting);
+                bgSetting.setBackgroundResource(R.color.setBlackBg);
+                TextView tv = (TextView) findViewById(R.id.txtVolumeIcon);
+                tv.setTextColor(getResources().getColor(R.color.setBlackfont));
+                tv = (TextView) findViewById(R.id.txtVolumeText);
+                tv.setTextColor(getResources().getColor(R.color.setBlackfont));
+                tv = (TextView) findViewById(R.id.txtVibrationIcon);
+                tv.setTextColor(getResources().getColor(R.color.setBlackfont));
+                tv = (TextView) findViewById(R.id.txtVibrationText);
+                tv.setTextColor(getResources().getColor(R.color.setBlackfont));
+                tv = (TextView) findViewById(R.id.tvSky);
+                tv.setTextColor(getResources().getColor(R.color.setBlackfont));
+                tv = (TextView) findViewById(R.id.tvStrawberry);
+                tv.setTextColor(getResources().getColor(R.color.setBlackfont));
+                tv = (TextView) findViewById(R.id.tvNight);
+                tv.setTextColor(getResources().getColor(R.color.setBlackfont));
+                //tvNight
+                //tvStrawberry
+                //tvSky
+            }
+
+        });
+    }
+    private void changeSetting (int in)
+    {
+
     }
 }
